5566704fb562fc2c5aff6797f2c7484fe6fade90
JulienTroudet <julien.troudet@gmail.com>
2012-08-13 16:15:24 +0200

    Moddification de l'interface admin
    L'interface d'administration à été modifiée afin de ne plus utiliser le
    Layout par défaut du module CRUD

--- app/controllers/Users.java
+++ app/controllers/Users.java
@@ -1,10 +1,14 @@
 package controllers;
 
 
+import java.util.List;
+
+import models.User;
+
 import play.mvc.With;
  
 @Check("admin")
 @With(Secure.class)
 public class Users extends CRUD {
-   
+
 }
